e4138633e15c5a734d1f6d3584760817d4be6cb3
Mathew Varghese <mathewvarghesemanu@gmail.com>
2021-05-08 02:09:45 +0530

    3 apis completed

--- Code/API/home.py
+++ Code/API/home.py
@@ -1,39 +1,58 @@
 from flask import Flask,request, jsonify
 import firebase_admin
 from firebase_admin import credentials,firestore
-
-cred = credentials.Certificate("E:/Work/activity_tracker_cube/Code/API/activity-tracker-cube-firebase-adminsdk-ugbiu-85cfbc3bbe.json")
+import os
+cwd=os.getcwd()
+complete_path=os.path.join(cwd,"activity-tracker-cube-firebase-adminsdk-ugbiu-85cfbc3bbe.json")
+print(complete_path)
+cred = credentials.Certificate(complete_path)
 firebase_admin.initialize_app(cred)
 db=firestore.client()
 
-
+activities=['playing','sleeping','studying','working']
 
 
 app=Flask(__name__)
 
-@app.route("/",methods =['POST'])
+@app.route("/",methods =["POST"])
 
 def data_ingestion():
     request_data=request.get_json()
-    
-    return 'Hi'
-    
-
-
-# # TODO: endpoint for receiving data in the format : {'user':'Mathew','activity':'exercise','starttime':10:30,'endtime':12:20,'duration':110}
-# # arrange data as username->data->...
-# @app.route('/data_ingestion')
-# def data_ingestion:
-#     data=request.json()
+    activity_data=jsonify(request_data).get_json()
+    activity_data.pop("user")
+    db.collection("activity_data").document(request_data["user"]).collection("data").add(activity_data)
+    return "Saved to firestore"
     
 
-# # TODO: endpoint for sending activities in the format : {'user':'Mathew','activity':['swimming','sleeping','dancing','learning']}
-# # arrange data as username -> activities -> ...
-# @app.route('/broadcast_activities')
+
+# TODO: endpoint for receiving data in the format : {"user":"Mathew","activity":"exercise","starttime":10:30,"endtime":12:20,"duration":110}
+# arrange data as username->data->...
+@app.route("/data_ingestion",methods =["POST"])
+def data_ingestion():
+    request_data=request.get_json()
+    activity_data=jsonify(request_data).get_json()
+    activity_data.pop("user")
+    db.collection("activity_data").document(request_data["user"]).collection("data").add(activity_data)
+    return "Saved to firestore"
+    
+
+# TODO: endpoint for sending activities in the format : {
+"user":"Mathew","activity":["swimming","sleeping","dancing","learning"]}
+# arrange data as username -> activities -> ...
+@app.route("/broadcast_activities")
+def get_activities:
+    return {'activities':activities}
 
 
-# # TODO: endpoint for sending activities in the format : {'user':'Mathew','activity':['swimming','sleeping','dancing','learning']}
-# # arrange data as username -> activities -> ...
-# @app.route('/get_activities')
+# TODO: endpoint for sending activities in the format : {"user":"Mathew","activity":["swimming","sleeping","dancing","learning"]}
+# arrange data as username -> activities -> ...
+@app.route("/get_activities")
+def set_activities():
+    request_data=request.get_json()
+    activity_list=jsonify(request_data).get_json()
+    activity_data.pop("user")
+    db.collection("activity_data").document(request_data["user"]).document("activity_list").add(activity_data)
+    return "Activities Saved to firestore"
+
 
 app.run(port=5000,  debug=True)
---
+++ Code/API/tees.py
@@ -0,0 +1,5 @@
+import os
+
+cwd=os.getcwd()
+complete_path=os.path.join(cwd,"Code\API","activity-tracker-cube-firebase-adminsdk-ugbiu-85cfbc3bbe.json")
+print(complete_path)
