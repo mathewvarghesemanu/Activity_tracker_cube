b35e51a8821948269c625d2bd07c4c64dce3041e
Mathew Varghese <mathewvarghesemanu@gmail.com>
2021-05-08 09:34:06 +0530

    API completed successfully

--- Code/API/home.py
+++ Code/API/home.py
@@ -14,20 +14,10 @@ activities=['playing','sleeping','studying','working']
 
 app=Flask(__name__)
 
-@app.route("/",methods =["POST"])
-
-def data_ingestion():
-    request_data=request.get_json()
-    activity_data=jsonify(request_data).get_json()
-    activity_data.pop("user")
-    db.collection("activity_data").document(request_data["user"]).collection("data").add(activity_data)
-    return "Saved to firestore"
-    
-
 
 # TODO: endpoint for receiving data in the format : {"user":"Mathew","activity":"exercise","starttime":10:30,"endtime":12:20,"duration":110}
 # arrange data as username->data->...
-@app.route("/data_ingestion",methods =["POST"])
+@app.route("/post_data",methods =["POST"])
 def data_ingestion():
     request_data=request.get_json()
     activity_data=jsonify(request_data).get_json()
@@ -37,21 +27,24 @@ def data_ingestion():
     
 
 # TODO: endpoint for sending activities in the format : {
-"user":"Mathew","activity":["swimming","sleeping","dancing","learning"]}
-# arrange data as username -> activities -> ...
-@app.route("/broadcast_activities")
-def get_activities:
-    return {'activities':activities}
+# {"activities":["swimming","sleeping","dancing","learning"]}
+@app.route("/get_activities/<string:person_name>",methods =["GET"])
+def get_activities():
+    activity_list = []
+    print(person_name)
+    received_activities = db.collection("activity_data").document(person_name).get().to_dict()
+    print(received_activities)
+    return received_activities
 
 
 # TODO: endpoint for sending activities in the format : {"user":"Mathew","activity":["swimming","sleeping","dancing","learning"]}
 # arrange data as username -> activities -> ...
-@app.route("/get_activities")
+@app.route("/post_activities")
 def set_activities():
     request_data=request.get_json()
     activity_list=jsonify(request_data).get_json()
-    activity_data.pop("user")
-    db.collection("activity_data").document(request_data["user"]).document("activity_list").add(activity_data)
+    activity_list.pop("user")
+    db.collection("activity_data").document(request_data["user"]).set(activity_list)
     return "Activities Saved to firestore"
 
 
---
+++ Code/nodemcu_test/nodemcu_test.ino
@@ -0,0 +1,25 @@
+#include <esp8266wifi.h>
+#include <esp8266httpclient.h>
+
+const char* ssid = "NETGEAR87";
+const char* password = "quaintsocks941";
+
+void setup () {
+ 
+  Serial.begin(115200);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+ 
+    delay(1000);
+    Serial.print("Connecting..");
+ 
+  }
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
