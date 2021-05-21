2478e455e9b17c9e9c907a663d38bc664917d454
Mathew Varghese <mathewvarghesemanu@gmail.com>
2021-05-21 10:16:46 +0530

    per day activity added to api

--- Code/API/home.py
+++ Code/API/home.py
@@ -3,21 +3,24 @@ import firebase_admin
 from firebase_admin import credentials,firestore
 import os
 import datetime
+
 cwd=os.getcwd()
 complete_path=os.path.join(cwd,"activity-tracker-cube-firebase-adminsdk-ugbiu-85cfbc3bbe.json")
 print(complete_path)
 cred = credentials.Certificate(complete_path)
 firebase_admin.initialize_app(cred)
 db=firestore.client()
+
 activities=["work","study","exercise","read"]
+
 
 app=Flask(__name__)
 
 @app.route("/")
 def test():
     return 'Activity tracker API'
-'''endpoint for receiving data in the format : {"user":"Mathew","activity":"exercise","starttime":"10:30","endtime":"12:20","duration":110}
- arrange data as username->data->...'''
+# TODO: endpoint for receiving data in the format : {"user":"Mathew","activity":"exercise","starttime":10:30,"endtime":12:20,"duration":110}
+# arrange data as username->data->...
 @app.route("/post_data",methods =["POST"])
 def data_ingestion():
     request_data=request.get_json()
@@ -27,7 +30,8 @@ def data_ingestion():
     return "Saved to firestore"
     
 
-'''endpoint for sending activities in the format : {"activities":["swimming","sleeping","dancing","learning"]}'''
+# TODO: endpoint for sending activities in the format : {
+# {"activities":["swimming","sleeping","dancing","learning"]}
 @app.route("/get_activities",methods=["GET"])
 def get_activities_helper():
     return "Enter name in URL to get activities"
@@ -41,7 +45,17 @@ def get_activities(person_name):
     print(received_activities)
     return received_activities
 
-#TODO : API to get the activity and corrresponding total time
+
+# TODO: endpoint for sending activities in the format : {"user":"Mathew","activity":["swimming","sleeping","dancing","learning"]}
+# arrange data as username -> activities -> ...
+@app.route("/post_activities")
+def set_activities():
+    request_data=request.get_json()
+    activity_list=jsonify(request_data).get_json()
+    activity_list.pop("user")
+    db.collection("activity_data").document(request_data["user"]).set(activity_list)
+    return "Activities Saved to firestore"
+
 @app.route("/get_activities/<string:person_name>/aggregate_time",methods =["GET"])
 def get_aggregare_time(person_name):
     activity_set = set(activities)
@@ -58,16 +72,16 @@ def get_aggregare_time(person_name):
         for distict_activity in activity_set:
             if activity_dict["activity"]==distict_activity:
                 activity_duration[distict_activity]=activity_duration[distict_activity]+activity_dict["duration"]
-    
     return(activity_duration)
-
 
 @app.route("/get_activities/<string:person_name>/aggregate_time_today",methods =["GET"])
 def get_aggregate_time_today(person_name):
     activity_set = set(activities)
     activity_duration= dict()
-    received_activities = db.collection("activity_data").document(person_name).collection("data").get()
-
+    received_activities = db.collection("activity_data").document("Mathew").collection("data").get()
+    
+    date=str(datetime.datetime.now())[:10]
+    
     # for activity in received_activities:
     #     activity_dict=activity.to_dict()
     #     activity_set.add(activity_dict['activity'])
@@ -77,26 +91,14 @@ def get_aggregate_time_today(person_name):
     for activity in received_activities:
         activity_dict=activity.to_dict()
         for distict_activity in activity_set:
-            if activity_dict["activity"]==distict_activity and activity_dict["startdate"]==date:
+            if (activity_dict["activity"]==distict_activity) and (activity_dict.get("startdate")==date):
                 activity_duration[distict_activity]=activity_duration[distict_activity]+activity_dict["duration"]
     
-    return(activity_duration!=null?activity_duration:"")
-
-
-'''endpoint for sending activities in the format : {"user":"Mathew","activity":["swimming","sleeping","dancing","learning"]}'''
-# arrange data as username -> activities -> ...
-@app.route("/post_activities")
-def set_activities():
-    request_data=request.get_json()
-    activity_list=jsonify(request_data).get_json()
-    activity_list.pop("user")
-    db.collection("activity_data").document(request_data["user"]).set(activity_list)
-    return "Activities Saved to firestore"
-
-
-# app.run(port=5000,  debug=True)
+    return(activity_duration)
 
 @app.route("/Date")
 def get_date():
     date=str(datetime.datetime.now())[:10]
     return {"date":date}
+    
+# app.run(port=5000,  debug=True)
